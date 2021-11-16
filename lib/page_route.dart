7bdcc0f338d1b7883b9cb75a4fdf1e8824c95950
jihun-24k <gi020477@gmail.com>
2021-11-16 21:16:41 +0900

    ToDo 리스트 동적 경로 설정

--- lib/page_route.dart
+++ lib/page_route.dart
@@ -2,29 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'page_main.dart';
+import 'page_create.dart';
 import 'page_detail.dart';
-import 'state_simple.dart';
+import 'page_state.dart';
 
 void main() => runApp(
     ChangeNotifierProvider(
       create: (context) => SimpleState(),
-      child: StateLoginDemo(),
+      child: StateList(),
     ));
 
 const String ROOT_PAGE = '/';
-const String MAIN_PAGE = '/main';
+const String CREATE_PAGE = '/create';
+const String DETAIL_PAGE = '/detail';
 
-class StateLoginDemo extends StatelessWidget{
+class StateList extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: '로그인 폼',
+      title: '투두투두투두',
       debugShowCheckedModeBanner: false,
       initialRoute: ROOT_PAGE,
       routes: {
-        ROOT_PAGE: (context) => LoginPage(),
-        MAIN_PAGE: (context) => MainPage(),
+        ROOT_PAGE: (context) => MainPage(),
+        CREATE_PAGE: (context) => CreatePage(),
+        DETAIL_PAGE: (context) => DetailPage(),
       },
     );
   }
---
+++ lib/page_state.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class SimpleState extends ChangeNotifier{
+  String _name;
+
+
+}
